ae6014c83c2d7b41d676c29e1498bc5c53bc6ef8
lienax <lilygoyao@hotmail.com>
2020-09-26 21:08:45 +0800

    New class: minesweeper

--- minesweeper/Form1.cs
+++ minesweeper/Form1.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using minesweeper.Properties;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -13,260 +15,139 @@ namespace minesweeper
     public partial class Form1 : Form
     {
         PictureBox[,] square;
-        int column;
-        int row;
+        Minesweeper game;
         public Form1()
         {
             InitializeComponent();
-            this.Size = new Size(System.Windows.Forms.Screen.PrimaryScreen.Bounds.Width, System.Windows.Forms.Screen.PrimaryScreen.Bounds.Height);
-            column = System.Windows.Forms.Screen.PrimaryScreen.Bounds.Width / 70;
-            row = System.Windows.Forms.Screen.PrimaryScreen.Bounds.Height / 70;
-            int[,] mine = new int[column, row];
-            Random rnd = new Random();
-            for (int totalMine = rnd.Next(((column * row) / 10), ((column * row) / 8)); totalMine > 0;) //setMine
+
+            int column = Screen.PrimaryScreen.WorkingArea.Width / 70;
+            int row = (Screen.PrimaryScreen.WorkingArea.Height - 70) / 70;
+            this.Size = new Size(column * 70 + 20, (row + 1) * 70 + 40);//set window size
+
+            game = new Minesweeper(column, row);
+
+            this.Controls.Add(new PictureBox //show mine icon on top
             {
-                Console.WriteLine("totalMine = {0}", totalMine);
-                int x = rnd.Next(0, column - 1);
-                int y = rnd.Next(0, row - 1);
-                if (mine[x, y] == 0)
-                {
-                    mine[x, y] = 9;
-                    totalMine--;
-                }
-            }
+                Location = new Point(Screen.PrimaryScreen.WorkingArea.Width / 2 - 70, 2),
+                Size = new Size(68, 68),
+                BackgroundImage = Image.FromFile("mine9.jpg"),
+                BackgroundImageLayout = ImageLayout.Zoom
+            });
+            this.Controls.Add(new Label //show mine number on top
+            {
+                Location = new Point(Screen.PrimaryScreen.WorkingArea.Width / 2, 2),
+                Text = String.Format("{0}", game.totalMine),
+                Font = new Font("Arial", 45, FontStyle.Bold),
+                AutoSize = true
+            });
+
             square = new PictureBox[column, row];
-            for (int x = 0; x < column - 1; x++)
+
+            for (int x = 0; x < column; x++)
             {
-                for (int y = 0; y < row - 1; y++)
+                for (int y = 0; y < row; y++)
                 {
-                    if (mine[x, y] < 9)
+                    square[x, y] = new PictureBox
                     {
-                        int nearbyMine = 0;
-                        for (int i = -1; i < 2; i++)
-                        {
-                            for (int j = -1; j < 2; j++)
-                            {
-                                if (!(x + i < 0 || x + i > column - 1 || y + j < 0 || y + j > row - 1))
-                                {
-                                    if (mine[x + i, y + j] > 8)
-                                    {
-                                        nearbyMine++;
-                                    }
-                                }
-                            }
-                        }
-                        mine[x, y] = nearbyMine;
-                    }
-                    square[x, y] = new PictureBox();
-                    square[x, y].Location = new System.Drawing.Point((70 * x) + 2, (70 * y) + 2);
-                    square[x, y].Size = new System.Drawing.Size(68, 68);
-                    square[x, y].BackColor = Color.Azure;
-
-                    pictureBoxTag tag = new pictureBoxTag();
-                    tag.init(x, y, mine[x, y]);
-                    square[x, y].Tag = tag;
+                        Location = new Point((70 * x) + 2, (70 * y) + 72),
+                        Size = new Size(68, 68),
+                        Tag = new pictureBoxTag(x, y),
+                        BackgroundImage = Image.FromFile("inactive.jpg"),
+                        BackgroundImageLayout = ImageLayout.Zoom,
+                        BackColor = Color.LightGray
+                    };
                     square[x, y].MouseClick += new MouseEventHandler(mouseClickEvent);
-                    square[x, y].BackgroundImage = System.Drawing.Image.FromFile("inactive.jpg");
-                    square[x, y].BackgroundImageLayout = ImageLayout.Zoom;
                     this.Controls.Add(square[x, y]);
-
                 }
             }
         }
-        struct pictureBoxTag
+        struct pictureBoxTag //put coordinates into tag of picturebox
         {
             public int pictureBox_x;
             public int pictureBox_y;
-            public int mineNum;
-
-            public void init(int x, int y, int num)
+            public pictureBoxTag(int x, int y)
             {
                 pictureBox_x = x;
                 pictureBox_y = y;
-                mineNum = num;
                 return;
             }
         }
-
         void mouseClickEvent(object sender, MouseEventArgs e)
         {
-            PictureBox pictureBox = (PictureBox)sender;
-            pictureBoxTag tag = (pictureBoxTag)pictureBox.Tag;
-            int mineNum = tag.mineNum;
-            Console.WriteLine("{0} {1}", tag.pictureBox_x, tag.pictureBox_y);
-            switch (e.Button)
-            {
-                case MouseButtons.Left:
-                    if (pictureBox.BackColor != Color.Yellow) //yellow means flag
-                    {
-                        switch (mineNum)
-                        {
-                            case 0: //no nearby mines
-                                pictureBox.BackColor = Color.LightGray;
-                                pictureBox.BackgroundImage = null;
-                                findZeros(tag.pictureBox_x, tag.pictureBox_y);
-                                break;
-                            case 1:
-                                pictureBox.BackColor = Color.LightGray;
-                                pictureBox.BackgroundImage = System.Drawing.Image.FromFile("mine1.jpg");
-                                pictureBox.BackgroundImageLayout = ImageLayout.Zoom;
-                                break;
-                            case 2:
-                                pictureBox.BackColor = Color.LightGray;
-                                pictureBox.BackgroundImage = System.Drawing.Image.FromFile("mine2.jpg");
-                                pictureBox.BackgroundImageLayout = ImageLayout.Zoom;
-                                break;
-                            case 3:
-                                pictureBox.BackColor = Color.LightGray;
-                                pictureBox.BackgroundImage = System.Drawing.Image.FromFile("mine3.jpg");
-                                pictureBox.BackgroundImageLayout = ImageLayout.Zoom;
-                                break;
-                            case 4:
-                                pictureBox.BackColor = Color.LightGray;
-                                pictureBox.BackgroundImage = System.Drawing.Image.FromFile("mine4.jpg");
-                                pictureBox.BackgroundImageLayout = ImageLayout.Zoom;
-                                break;
-                            case 5:
-                                pictureBox.BackColor = Color.LightGray;
-                                pictureBox.BackgroundImage = System.Drawing.Image.FromFile("mine5.jpg");
-                                pictureBox.BackgroundImageLayout = ImageLayout.Zoom;
-                                break;
-                            case 6:
-                                pictureBox.BackColor = Color.LightGray;
-                                pictureBox.BackgroundImage = System.Drawing.Image.FromFile("mine6.jpg");
-                                pictureBox.BackgroundImageLayout = ImageLayout.Zoom;
-                                break;
-                            case 7:
-                                pictureBox.BackColor = Color.LightGray;
-                                pictureBox.BackgroundImage = System.Drawing.Image.FromFile("mine7.jpg");
-                                pictureBox.BackgroundImageLayout = ImageLayout.Zoom;
-                                break;
-                            case 8:
-                                pictureBox.BackColor = Color.LightGray;
-                                pictureBox.BackgroundImage = System.Drawing.Image.FromFile("mine8.jpg");
-                                pictureBox.BackgroundImageLayout = ImageLayout.Zoom;
-                                break;
-                            case 9:
-                                pictureBox.BackColor = Color.Red;
-                                pictureBox.BackgroundImage = System.Drawing.Image.FromFile("mine9.jpg");
-                                pictureBox.BackgroundImageLayout = ImageLayout.Zoom;
-                                MessageBox.Show("You lose!");
-                                Application.Restart();
-                                break;
-                        }
-                    }
-                    break;
-                case MouseButtons.Right:
-                    if (pictureBox.BackColor == Color.Azure)
-                    {
-                        pictureBox.BackColor = Color.Yellow;
-                        pictureBox.BackgroundImage = System.Drawing.Image.FromFile("flag.jpg");
-                        pictureBox.BackgroundImageLayout = ImageLayout.Zoom;
-                    }
-                    else if (pictureBox.BackColor == Color.Yellow)
-                    {
-                        pictureBox.BackColor = Color.Azure;
-                        pictureBox.BackgroundImage = System.Drawing.Image.FromFile("inactive.jpg");
-                        pictureBox.BackgroundImageLayout = ImageLayout.Zoom;
-                    }
-                    break;
-            }
-        }
-        void findZeros(int x, int y)
-        {
-            if (x + 1 < column - 1 && square[x + 1, y].BackColor == Color.Azure)
-            {
-                pictureBoxTag tag = (pictureBoxTag)square[x + 1, y].Tag;
-                if (tag.mineNum == 0)
-                {
-                    square[x + 1, y].BackColor = Color.LightGray;
-                    square[x + 1, y].BackgroundImage = null;
-                    findZeros(x + 1, y);
-                }
-                else if (tag.mineNum < 9)
-                {
-                    square[x + 1, y].BackColor = Color.LightGray;
-                    square[x + 1, y].BackgroundImage = setPicture(tag.mineNum);
-                }
-            }
-            if (x - 1 > -1 && square[x - 1, y].BackColor == Color.Azure)
-            {
-                pictureBoxTag tag = (pictureBoxTag)square[x - 1, y].Tag;
-                if (tag.mineNum == 0)
-                {
-                    square[x - 1, y].BackColor = Color.LightGray;
-                    square[x - 1, y].BackgroundImage = null;
-                    findZeros(x - 1, y);
-                }
-                else if (tag.mineNum < 9)
-                {
-                    square[x - 1, y].BackColor = Color.LightGray;
-                    square[x - 1, y].BackgroundImage = setPicture(tag.mineNum);
-                }
-            }
-            if (y + 1 < row - 1 && square[x, y + 1].BackColor == Color.Azure)
-            {
-                pictureBoxTag tag = (pictureBoxTag)square[x, y + 1].Tag;
-                if (tag.mineNum == 0)
-                {
-                    square[x, y + 1].BackColor = Color.LightGray;
-                    square[x, y + 1].BackgroundImage = null;
-                    findZeros(x, y + 1);
-                }
-                else if (tag.mineNum < 9)
-                {
-                    square[x, y + 1].BackColor = Color.LightGray;
-                    square[x, y + 1].BackgroundImage = setPicture(tag.mineNum);
-                }
-            }
-            if (y - 1 > -1 && square[x, y - 1].BackColor == Color.Azure)
-            {
-                pictureBoxTag tag = (pictureBoxTag)square[x, y - 1].Tag;
-                if (tag.mineNum == 0)
-                {
-                    square[x, y - 1].BackColor = Color.LightGray;
-                    square[x, y - 1].BackgroundImage = null;
-                    findZeros(x, y - 1);
-                }
-                else if (tag.mineNum < 9)
-                {
-                    square[x, y - 1].BackColor = Color.LightGray;
-                    square[x, y - 1].BackgroundImage = setPicture(tag.mineNum);
-                }
-            }
-            return;
-        }
+            pictureBoxTag tag = (pictureBoxTag)((PictureBox)sender).Tag;
 
-        Image setPicture(int mine)
-        {
-            switch (mine)
+            switch (e.Button)//mouse click button check
             {
+                case MouseButtons.Left://left click
+                    List<Minesweeper.updateUI> leftClickUpdate = game.leftClick(tag.pictureBox_x, tag.pictureBox_y);//get list of ui update
+                    if(leftClickUpdate.Count!=0 && leftClickUpdate[0].newState == 9)//check game over
+                    {
+                        square[leftClickUpdate[0].ui_x, leftClickUpdate[0].ui_y].BackgroundImage = setPicture(leftClickUpdate[0].newState);
+                        if (MessageBox.Show("You lose!", "", MessageBoxButtons.RetryCancel) == DialogResult.Retry)
+                            Application.Restart();
+                        else
+                            this.Close();
+                        break;
+                    }
+                    for (int i = 0; i < leftClickUpdate.Count(); i++)//update ui
+                    {
+                        square[leftClickUpdate[i].ui_x, leftClickUpdate[i].ui_y].BackgroundImage = setPicture(leftClickUpdate[i].newState);
+                        square[leftClickUpdate[i].ui_x, leftClickUpdate[i].ui_y].MouseClick -= mouseClickEvent;
+                    }
+                    if(game.checkVictory())//check victory
+                    {
+                        if (MessageBox.Show("You win!", "", MessageBoxButtons.RetryCancel) == DialogResult.Retry)
+                            Application.Restart();
+                        else
+                            this.Close();
+                    }
+                    break;//end left click
+                case MouseButtons.Right://right click (flag / unflag)
+                    PictureBox pictureBox = (PictureBox)sender;
+                    if (game.rightClick(tag.pictureBox_x, tag.pictureBox_y))
+                    {
+                        pictureBox.BackgroundImage = Image.FromFile("flag.jpg");
+                    }
+                    else
+                    {
+                        pictureBox.BackgroundImage = Image.FromFile("inactive.jpg");
+                    }
+                    break;
+            }
+        }
+        Image setPicture(int tag)
+        {
+            switch (tag)
+            {
+                case 0:
+                    return null;
+                    break;
                 case 1:
-                    return System.Drawing.Image.FromFile("mine1.jpg");
+                    return Image.FromFile("mine1.jpg");
                     break;
                 case 2:
-                    return System.Drawing.Image.FromFile("mine2.jpg");
+                    return Image.FromFile("mine2.jpg");
                     break;
                 case 3:
-                    return System.Drawing.Image.FromFile("mine3.jpg");
+                    return Image.FromFile("mine3.jpg");
                     break;
                 case 4:
-                    return System.Drawing.Image.FromFile("mine4.jpg");
+                    return Image.FromFile("mine4.jpg");
                     break;
                 case 5:
-                    return System.Drawing.Image.FromFile("mine5.jpg");
+                    return Image.FromFile("mine5.jpg");
                     break;
                 case 6:
-                    return System.Drawing.Image.FromFile("mine6.jpg");
+                    return Image.FromFile("mine6.jpg");
                     break;
                 case 7:
-                    return System.Drawing.Image.FromFile("mine7.jpg");
+                    return Image.FromFile("mine7.jpg");
                     break;
                 case 8:
-                    return System.Drawing.Image.FromFile("mine8.jpg");
+                    return Image.FromFile("mine8.jpg");
                     break;
                 default:
-                    return System.Drawing.Image.FromFile("mine9.jpg");
+                    return Image.FromFile("mine9.jpg");
                     break;
 
             }
---
+++ minesweeper/Properties/Minesweeper.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace minesweeper.Properties
+{
+    class Minesweeper
+    {
+        struct mine
+        {
+            public int tagNum;//9 means mine
+            public bool flag;//flag: true, unflag: false
+            public bool exposed;//clicked: true, unclicked: false
+        }
+
+        public struct updateUI
+        {
+            public int ui_x;
+            public int ui_y;
+            public int newState;//same with tagNum
+
+            public updateUI(int x, int y, int state)
+            {
+                ui_x = x;
+                ui_y = y;
+                newState = state;
+            }
+        }
+
+        mine[,] mineArray;//game map
+        int victoryCountDown;//victory when 0
+        public int totalMine;//number of mine in total
+        public Minesweeper(int column, int row)
+        {
+            mineArray = new mine[column, row];
+            Random rnd = new Random();
+            totalMine = rnd.Next(((column * row) / 10), ((column * row) / 8));//random number of mine
+            victoryCountDown = column * row - totalMine;
+            Console.WriteLine("Mine count: {0}", totalMine);
+            Console.WriteLine("Col {0}*Row {1}: {2}", column, row, column * row);
+            Console.WriteLine("victoryCountDown: {0}", victoryCountDown);
+            for (int mineGenerate = totalMine; mineGenerate > 0;) //generate mines
+            {
+                int x = rnd.Next(0, column - 1);
+                int y = rnd.Next(0, row - 1);
+                if (mineArray[x, y].tagNum == 0)
+                {
+                    mineArray[x, y].tagNum = 9;
+                    mineGenerate--;
+                }
+            }
+
+            for (int x = 0; x < column; x++)//calculate tagNum of remaining squares (non mine areas)
+            {
+                for (int y = 0; y < row; y++)
+                {
+                    if (mineArray[x, y].tagNum < 9)
+                    {
+                        int nearbyMine = 0;
+                        for (int i = -1; i < 2; i++)
+                        {
+                            for (int j = -1; j < 2; j++)
+                            {
+                                if (!(x + i < 0 || x + i > column - 1 || y + j < 0 || y + j > row - 1))
+                                {
+                                    if (mineArray[x + i, y + j].tagNum == 9)
+                                    {
+                                        nearbyMine++;
+                                    }
+                                }
+                            }
+                        }
+                        mineArray[x, y].tagNum = nearbyMine;
+                    }
+                }
+            }
+        }
+        public List<updateUI> leftClick(int x, int y)//left click event
+        {
+            List<updateUI> ui = new List<updateUI>();
+            if (!mineArray[x, y].flag && !mineArray[x, y].exposed)
+            {
+                ui.Add(new updateUI(x, y, mineArray[x, y].tagNum));
+                mineArray[x, y].exposed = true;
+                victoryCountDown--;
+                if (mineArray[x, y].tagNum == 0)//expose nearby tiles when 0
+                {
+                    ui.AddRange(findZeros(x, y));
+                }
+            }
+            return ui;
+        }
+        public bool rightClick(int x, int y)//flag / unflag
+        {
+            mineArray[x, y].flag = !mineArray[x, y].flag;
+            return mineArray[x, y].flag;
+        }
+        List<updateUI> findZeros(int x, int y)//search nearby tiles until non zero is found
+        {
+            List<updateUI> ui = new List<updateUI>();
+            int column = mineArray.GetLength(0);
+            int row = mineArray.GetLength(1);
+
+            if (x + 1 < column && !mineArray[x + 1, y].flag && !mineArray[x + 1, y].exposed)
+            {
+                ui.Add(new updateUI(x + 1, y, mineArray[x + 1, y].tagNum));
+                mineArray[x + 1, y].exposed = true;
+                victoryCountDown--;
+                if (mineArray[x + 1, y].tagNum == 0)
+                    ui.AddRange(findZeros(x + 1, y));
+            }
+            if (x - 1 > -1 && !mineArray[x - 1, y].flag && !mineArray[x - 1, y].exposed)
+            {
+                ui.Add(new updateUI(x - 1, y, mineArray[x - 1, y].tagNum));
+                mineArray[x - 1, y].exposed = true;
+                victoryCountDown--;
+                if (mineArray[x - 1, y].tagNum == 0)
+                    ui.AddRange(findZeros(x - 1, y));
+            }
+            if (y + 1 < row && !mineArray[x, y + 1].flag && !mineArray[x, y + 1].exposed)
+            {
+                ui.Add(new updateUI(x, y + 1, mineArray[x, y + 1].tagNum));
+                mineArray[x, y + 1].exposed = true;
+                victoryCountDown--;
+                if (mineArray[x, y + 1].tagNum == 0)
+                    ui.AddRange(findZeros(x, y + 1));
+            }
+            if (y - 1 > -1 && !mineArray[x, y - 1].flag && !mineArray[x, y - 1].exposed)
+            {
+                ui.Add(new updateUI(x, y - 1, mineArray[x, y - 1].tagNum));
+                mineArray[x, y - 1].exposed = true;
+                victoryCountDown--;
+                if (mineArray[x, y - 1].tagNum == 0)
+                    ui.AddRange(findZeros(x, y - 1));
+            }
+            return ui;
+        }
+        public bool checkVictory()//return victory is true when countdown equals zero
+        {
+            Console.WriteLine("Victory Count Down: {0}", victoryCountDown);
+            if (victoryCountDown == 0)
+                return true;
+            else
+                return false;
+        }
+
+    }
+}
